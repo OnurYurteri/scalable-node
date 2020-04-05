2ee0fe0d2db155e402ec2bc50b44e5b04e5db4fc
Onur Yurteri <onur.yurteri@inomera.com>
2020-04-05 17:08:53 +0300

    Remove files on project root

--- app/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const AppController = require('./controller');
-
-router.get('/login', AppController.login);
-
-module.exports = router;
